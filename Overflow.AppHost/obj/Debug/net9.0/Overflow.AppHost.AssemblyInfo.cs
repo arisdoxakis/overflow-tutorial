9734a5cb406fecf412ae4ae262411b4af2aba0c8
arisdoxakis <arisdoxakis@gmail.com>
2025-10-24 05:38:45 -0700

    end of section 2

--- Overflow.AppHost/obj/Debug/net9.0/Overflow.AppHost.AssemblyInfo.cs
+++ Overflow.AppHost/obj/Debug/net9.0/Overflow.AppHost.AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Overflow.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be8b5bf184949e00c5e3cc2dd3aa9d88516c91e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Overflow.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Overflow.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
